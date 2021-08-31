b15b58ee58ba4196d8ce576509c9e4f47709090d
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2021-08-31 02:02:11 -0500

    content json

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export default {
     // api: 'http://127.0.0.1:8000/',
     api: 'https://subsidios-thanos-backend.softys-leadcorporate.com/',
-    urlFrontend: 'http://localhost:3000',
-    // urlFrontend: 'https://subsidios-thanos.softys-leadcorporate.com',
+    // urlFrontend: 'http://localhost:3000',
+    urlFrontend: 'https://subsidios-thanos.softys-leadcorporate.com',
     basename: '/',
-    produccion: false,
+    produccion: true,
 };
