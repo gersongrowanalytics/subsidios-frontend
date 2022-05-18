c7ece50c08ea00acd4a50a6aeed5b078709299c8
gerson.vilca@grow-analytics.com <gerson.vilca@grow-analytics.com>
2022-05-18 00:32:05 -0500

    build

--- src/config.js
+++ src/config.js
@@ -1,13 +1,12 @@
 export default {
     // api: 'http://192.168.100.5:8000/',
-    api: 'http://192.168.100.24:8000/',
     // api: 'https://comosea.dir.tips/',
-    // api: 'https://subsidios-thanos-backend.softys-leadcorporate.com/',
-    api: 'http://192.168.137.102:8000/',
-    urlFrontend: 'http://localhost:3000',
-    // urlFrontend: 'https://subsidios-thanos.softys-leadcorporate.com',
+    api: 'https://subsidios-thanos-backend.softys-leadcorporate.com/',
+    // api: 'http://192.168.1.8:8000/',
+    // urlFrontend: 'http://localhost:3000',
+    urlFrontend: 'https://subsidios-thanos.softys-leadcorporate.com',
     basename: '/',
-    produccion: false,
+    produccion: true,
 
     nombreSistema : 'Subsidios',
     nombreSistemaHo : 'Subsidios',
